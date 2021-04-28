6369bb308ac2c6457b5a0af2511e64a05767544c
C. Praise Anyanwu <anyanc@uw.edu>
2021-04-28 15:09:18 -0700

    test case for curve fit module, assert number of valid parameters

---
+++ 3dLiIBattery/test/test_fit3dliib.py
@@ -0,0 +1,37 @@
+import os
+
+import fit3dliib
+
+
+def test_fit():
+    """
+    Test case for curve fit module to 3d lithium ion battery
+    test the output size in an array of three elements corresponding
+    to the optimized, desired, paramters: the characteristic time tau,
+    the n fractor, and specific capacity Q
+    """
+    filepath = os.path.join("data", "/capacityratepaper1set1.csv")
+    # parameters
+    tau = 0.5
+    n = 1
+    Qcapacity = 100
+    params0 = [tau, n, Qcapacity]  # intial guess parameter
+    popt = fit3dliib.fit(filepath, params0)
+    try:
+        lenparam = len(popt)
+        assert(lenparam == 3)
+    except AssertionError:
+        print(f'I should have only three parameters, \
+              not {lenparam}')
+    return
+
+
+# def test_fitfunc():
+# 	'''
+# 	The output of the fit function is the (mass) normalize capacity
+# 	simply assert that the size of the output array is equal to the
+# 	size of the input array, and that
+# 	the maximum value at initail discharge rate (at zero) is equal to
+# 	the specific capacity
+# 	'''
+# 	return
