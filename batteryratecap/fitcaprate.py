8f2fe293cd591a1ab153c67fcf28ac96b0ff4771
Hung,Chih-Hsuan <dhung@uw.edu>
2022-12-04 22:40:41 -0800

    update to remove data dependency and update funtion names

--- batteryratecap/fitcaprate.py
+++ batteryratecap/fitcaprate.py
@@ -1,33 +1,22 @@
 import numpy as np
 import os
 import re
-
 import pandas as pd
 from scipy.optimize import curve_fit
-
-# fit battery capacity-rate dataset
-# data = os.path.join(os.getcwd(), 'liibattery3d')
-pathtomodule = os.path.abspath(__file__)
-dirname = os.path.dirname(pathtomodule)
-data = os.path.join(dirname, 'data')
+import matplotlib 
+from matplotlib import pyplot as plt
 
 
-def fitmodel():
+def fitmodel(dframe, output_dir, params0):
     '''
-    A curve fitting procedure to determine the chracteristic time, 
-    the exponent *n*, and the maximum capacity (Q_max).
-    We use the model outlined
-    by *R. Tian & S. Park et. al.*
-    https://www.nature.com/articles/s41467-019-09792-9
-    Here we fit all dataset in this package, and the we
-    write the output of the optimized fit parameters, for
-    each dataset, in a csv file
+    This function fits capacity-rate dataframe and outputs
+    the optimized fit parameters and covaraiances in a csv file.
+    Inputs
+    - capacity-rate dataframe
+    - output directory for the csv file that contains fitting parameters 
+    - params0: a list of initial points for searching [tau, n, Qmax]
+    The data fitting is done using the fit() function below in this file.
     '''
-    # load dataset
-    filepath = os.path.join(data, "performancelog.xls")
-    dframe = pd.read_excel(filepath, sheet_name='3_CapacityRate',
-                           header=[0, 1, 2])
-
     # Fit procedure
     # define list of fit parameters and of their standard deviations
     taus = []
@@ -49,11 +38,6 @@ def fitmodel():
         # fit dataset with more than four datapoints
         # otherwise, discard
         if len(Rdischarge) >= 4:
-            # parameters
-            tau = 0.5
-            n = 1
-            specificQ = 100
-            params0 = [tau, n, specificQ]  # intial guess parameter
             popt, pcov = fit(params0, xdata=Rdischarge, ydata=normQ)
             tau, n, specificQ = popt
             # standard deviation
@@ -103,24 +87,15 @@ def fitmodel():
         popt_dframe = popt_dframe.append(row_series, ignore_index=True)
 
     # Write dataframe of optimized parameters to csv file
-    outputpath = os.path.join(data, "fitparameters.csv")
+    outputpath = os.path.join(output_dir, "fitparameters.csv")
     popt_dframe.to_csv(path_or_buf=outputpath, index=False)
     return
 
-# define fit procedure
-
-
 def fit(params0, **kwargs):
     '''
-    A curve fitting procedure to determine the discharge
-    rate constant, the n factor for the discharge rate,
-    and the specific capacity. We use the model outlined
-    by *R. Tian & S. Park et. al.*
-    https://www.nature.com/articles/s41467-019-09792-9
-    Input Arguments
-    params0: 1 by 3 numpy array, with elemets, tau (the
-    characteristic lifetime), n (the rate discharge coef-
-    ficient), and Q the specific capacity
+    This function fits capacity-rate data to an empirical model and outputs
+    the optimized fit parameters (chracteristic time, 
+    the exponent *n*, and the maximum capacity) and their covariances.
     Keyword Arguments
     xdata: 1D numpy array, default as 'Rdischarge'
     ydata: 1D numpy array, default as normQ
@@ -138,17 +113,13 @@ def fit(params0, **kwargs):
         dframe = pd.read_csv(filepath)
         Rdischarge = dframe.iloc[:, 0].to_numpy()
         normQ = dframe.iloc[:, 1].to_numpy()
-
     # Fit procedure
     popt, pcov = curve_fit(fitfunc, Rdischarge, normQ, p0=params0)
     return popt, pcov
 
-# define fit function
-
-
 def fitfunc(Rdischarge, tau, n, specificQ):
     '''
-    This is the empirical model outlined by
+    This is the empirical model developed by Tian et al.(2019):
     https://www.nature.com/articles/s41467-019-09792-9
     '''
     normQ = specificQ * (1 -
@@ -156,3 +127,59 @@ def fitfunc(Rdischarge, tau, n, specificQ):
                          (1 - np.exp(- (Rdischarge * tau)**(- n)))
                          )
     return normQ
+
+def plotfit(dframe, dframe_out):
+    '''
+    This function fits and plots capacity-rate data and their fitting results as a panel figure.
+    '''
+    # plot for predicted capacity (with optimized parameters) compared to known (measured) capacity outputs
+    matplotlib.rcParams.update({'font.size': 18})
+    fig2 = plt.figure(figsize=(20, 20))
+    for i in range(int(len(dframe.columns)/2)):
+        # test data
+        # even columns for xdata; odd columns for ydata
+        # then sort values by the even(th) columns
+        indeven = (2 * i)
+        indodd = indeven + 1
+        data = dframe.iloc[:, [indeven, indodd]].sort_values(by=dframe.columns[indeven])
+        xdata = data[dframe.columns[indeven]].to_numpy()
+        ydata = data[dframe.columns[indodd]].to_numpy()
+        #Prediction
+        # optimized parameters
+        tau = dframe_out.loc[i, "tau"]
+        n = dframe_out.loc[i, "n"]
+        specificQ = dframe_out.loc[i, "Q"]
+        # error margins of optimized parameters
+        sigma_tau = dframe_out.loc[i, "sigma_tau"]
+        sigma_n = dframe_out.loc[i, "sigma_n"]
+        sigma_Q = dframe_out.loc[i, "sigma_Q"]
+        # use fit parameters to predict capacity
+        Rdischarge = np.linspace(min(xdata), max(xdata), 100)
+        normQopt = fitfunc(Rdischarge, tau, n, specificQ)  # call model function
+        # compare predicted capacity to known capacity in ydata
+        ax = plt.subplot(5, 4, (i + 1),
+                         title= '\nSE =' + f'{sigma_tau:.2f}'+ ','
+                         + f'{sigma_n:.2f}'+ ','
+                         + f'{sigma_Q:.2f}')  # five rows and four columns
+        ax.plot(xdata, ydata,
+                color='b', marker='o',
+                markersize=5, linestyle='None',
+                label='data')
+        # consider only prediction of dataset with more than four data points
+        if not (tau==0 and n==0 and specificQ==0):
+            ax.plot(Rdischarge, normQopt, 
+                    color='r',
+                    linewidth=2, linestyle='--',
+                    label='lsqcurvefit')
+            labels = '#'+str(i)+'\ntau = ' + \
+                f'{tau:.2f}' +'\nn = ' + \
+                f'{n:.2f}' + '\nQ = ' + \
+                f'{specificQ:.2f}'
+            plt.text(0.1, 0.1, labels, transform=ax.transAxes)
+        else:
+            plt.text(0.1, 0.1, 'not enough datapoints', transform=ax.transAxes)
+        plt.legend(markerscale=0.5, fontsize='xx-small', loc='upper right')
+        ax.set_xlabel('discharge rate')
+        ax.set_ylabel('capacity [mAh/g]') # mass normalized capacity
+        plt.tight_layout()
+    return
