92036a199c70864fc2957676bb3cedb85f5e71bf
Doris Hung <dhung@uw.edu>
2023-05-30 14:08:45 -0700

    pep8 compliant

--- batteryratecap/__init__.py
+++ batteryratecap/__init__.py
@@ -1,5 +1,1 @@
-from .fitcaprate import fitmodel, fit, fitfunc, plotfit
-from .visualization import feature_vs_n_tau_q
-from .correlationtest import correlation_hypothesis, plot_linear_regression, linear_outliers
-from .data_converter import potential_rate_paper_set, potential_rate_all, excel_merge, capacity_cycle
-__all__ = [fitmodel, fit, fitfunc, plotfit, feature_vs_n_tau_q, correlation_hypothesis, plot_linear_regression, linear_outliers, potential_rate_paper_set, potential_rate_all, excel_merge, capacity_cycle]
+# This is the __init.py__ file for batteryratecap package
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+# This is in the __init__.py file for the tests folder
